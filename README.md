9b083fba2d5b5e076eaed4963e53119c1a8c36ed
RunaNevermind <79098228+RunaNevermind@users.noreply.github.com>
2022-08-03 14:29:21 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# StarWars-data
+ Test
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  mode:'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+				test: /\.(sa|sc|c)ss$/,
+				use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+					'sass-loader',          
+				],
+			},
+      {
+        test:/\.(|svg|jpg|jpeg|png|gif)$/i,
+        type:'asset/resource',
+        generator:{
+          filename:'assets/img/[name]_[hash][ext]',
+        },
+      },      
+      {
+        test:/fonts.*\.(woff|woff2|ttf)$/i,
+        type:'asset/resource',
+        generator:{
+          filename:'assets/fonts/[name]_[hash][ext]',
+        },
+      },
+
+    ],
+  },  
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename:'[name].min.css',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
