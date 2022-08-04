010fe7414660db3d9e048fdc6f4e475899dd8bf2
RunaNevermind <79098228+RunaNevermind@users.noreply.github.com>
2022-08-04 15:11:12 +0500

    check angularJs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import './index.scss';
 import * as bootstrap from 'bootstrap'
 
+var e = document.querySelector(".form-select");
+var val = e.options[e.selectedIndex].value;
+
+angular.module("apiHandler",[])
+.controller("apiCtrl", function($scope){
+    $scope.apiVal=val;
+});
 
 
 /*
