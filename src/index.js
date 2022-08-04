96507c6988f4209cfb46ebf2a4b8c67fb6d3aaaf
RunaNevermind <79098228+RunaNevermind@users.noreply.github.com>
2022-08-04 18:48:00 +0500

    massive is get

--- src/index.js
+++ src/index.js
@@ -1,14 +1,43 @@
 import './index.scss';
 import * as bootstrap from 'bootstrap'
 
+var baseUrl ="https://www.swapi.tech/api/";
 var e = document.querySelector(".form-select");
 var val = e.options[e.selectedIndex].value;
 
+
 angular.module("apiHandler",[])
 .controller("apiCtrl", function($scope){
-    $scope.apiVal=val;
-});
 
+    $scope.apiVal=val;
+    $scope.apiName="";
+    $scope.apiWookie="";
+    $scope.result=""
+
+    
+    $scope.find=function(){
+        if($scope.apiWookie===true){
+            $scope.apiWookie="?format=wookiee"
+        }
+        if(/\D/.test($scope.apiName)){
+            console.log("name")
+            console.log(baseUrl+$scope.apiVal+"/?name="+$scope.apiName+$scope.apiWookie)
+            fetch(baseUrl+$scope.apiVal+"/?name="+$scope.apiName+$scope.apiWookie)
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.error(err))            
+        }
+        else{
+            console.log("id")                
+            console.log(baseUrl+$scope.apiVal+"/"+$scope.apiName+$scope.apiWookie)
+            fetch(baseUrl+$scope.apiVal+"/"+$scope.apiName+$scope.apiWookie)
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.error(err))            
+        }
+ 
+    }
+});
 
 /*
 fetch("https://www.swapi.tech/api/people/")
