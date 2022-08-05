3c851847b74364c885622709c5950bc77d924f19
RunaNevermind <79098228+RunaNevermind@users.noreply.github.com>
2022-08-05 19:07:15 +0500

    work with api

--- src/index.js
+++ src/index.js
@@ -1,45 +1,43 @@
 import './index.scss';
 import * as bootstrap from 'bootstrap'
 
-var baseUrl ="https://www.swapi.tech/api/";
-var e = document.querySelector(".form-select");
+var baseUrl ='https://www.swapi.tech/api/';
+var e = document.querySelector('.form-select');
 var val = e.options[e.selectedIndex].value;
 
 
-angular.module("apiHandler",[])
-.controller("apiCtrl", function($scope){
+angular.module('apiHandler',[])
+.controller('apiCtrl', function($scope){
 
     $scope.apiVal=val;
-    $scope.apiName="";
-    $scope.apiWookie="";
-    $scope.result= $scope.result;
+    $scope.apiName='';
+    $scope.apiWookie='';
     $scope.arrResult=[];
-    
 
 
 
-    $scope.find=function(){
+    $scope.find= function(){
         if($scope.apiWookie===true){
-            $scope.apiWookie="?format=wookiee"
+            $scope.apiWookie='?format=wookiee'
         }
-
-        if($scope.apiName===""){
+        
+        if($scope.apiName===''){
             console.log('mass')
-            console.log(baseUrl+$scope.apiVal+"/"+$scope.apiName+$scope.apiWookie)
-            fetch(baseUrl+$scope.apiVal+"/"+$scope.apiName+$scope.apiWookie)
+            console.log(baseUrl+$scope.apiVal+'/'+$scope.apiName+$scope.apiWookie)
+            fetch(baseUrl+$scope.apiVal+'/'+$scope.apiName+$scope.apiWookie)
             .then(res => res.json())
             .then(data => {
                 if($scope.apiVal==='films'){
                     for(var i = 0; i < data.result.length; i++){
-                        console.log(data.result[i].properties.title)
-                        $scope.arrResult.push(data.result[i].name);     
+                        console.log(data.result[i].properties)
+                        $scope.arrResult.push(data.result[i]);     
                     }
 
                 }
                 else{
                     for(var i = 0; i < data.results.length; i++){
-                        console.log(data.results[i].name)
-                        $scope.arrResult.push(data.results[i].name);                    
+                        console.log(data.results[i])
+                        $scope.arrResult.push(data.results[i]);                    
                     }
                 }    
             })
@@ -47,25 +45,24 @@ angular.module("apiHandler",[])
         }
         else{
             if(/\D/.test($scope.apiName)){
-                console.log('name')
-                console.log(baseUrl+$scope.apiVal+"/?name="+$scope.apiName+$scope.apiWookie)
-                fetch(baseUrl+$scope.apiVal+"/?name="+$scope.apiName+$scope.apiWookie)
+                console.log(baseUrl+$scope.apiVal+'/?name='+$scope.apiName+$scope.apiWookie)
+                fetch(baseUrl+$scope.apiVal+'/?name='+$scope.apiName+$scope.apiWookie)
                 .then(res => res.json())
                 .then(data => {
                     console.log(data.result.properties)
-                    $scope.result=data.result.properties;
+                    $scope.arrResult.push(data.result.properties);
                 })
                 .catch(err => console.error(err)) 
         
             }
             else{
                 console.log('id')
-                console.log(baseUrl+$scope.apiVal+"/"+$scope.apiName+"/"+$scope.apiWookie)
-                fetch(baseUrl+$scope.apiVal+"/"+$scope.apiName+"/"+$scope.apiWookie)
+                console.log(baseUrl+$scope.apiVal+'/'+$scope.apiName+'/'+$scope.apiWookie)
+                fetch(baseUrl+$scope.apiVal+'/'+$scope.apiName+'/'+$scope.apiWookie)
                 .then(res => res.json())
                 .then(data => {
                     console.log(data.result.properties)
-                    $scope.result=data.result.properties;
+                    $scope.result.push(data.result.properties);
                 })
                 .catch(err => console.error(err))    
            
@@ -77,7 +74,7 @@ angular.module("apiHandler",[])
 });
 
 /*
-fetch("https://www.swapi.tech/api/people/")
+fetch('https://www.swapi.tech/api/people/')
 .then(res => res.json())
 .then(data => console.log(data))
 .catch(err => console.error(err))
